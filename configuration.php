98b639e4464144dac381091a71334a5dd8f35cf1
sgaluzin <sgaluzin@gmail.com>
2015-03-03 20:04:39 +0300

    перенос на главную

--- configuration.php
+++ configuration.php
@@ -54,8 +54,8 @@ class JConfig {
 	public $sef_suffix = '0';
 	public $unicodeslugs = '0';
 	public $feed_limit = '10';
-	public $log_path = '/home/c/cs21439/public_html/joomla/logs';
-	public $tmp_path = '/home/c/cs21439/public_html/joomla/tmp';
+	public $log_path = '/home/c/cs21439/public_html/logs';
+	public $tmp_path = '/home/c/cs21439/public_html/tmp';
 	public $lifetime = '15';
 	public $session_handler = 'database';
 }
